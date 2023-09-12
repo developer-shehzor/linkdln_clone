15b8fdcc6fe86eebd372be4e6ad9615cc17cec37
ShehzorMemon <Shezymemon0180@gmail.com>
2023-09-12 14:29:16 +0500

    Almost completed the frontend of Main.js file.

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,4 +1,8 @@
 import styled from 'styled-components'
+import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
+import InsertCommentIcon from '@mui/icons-material/InsertComment';
+import SendIcon from '@mui/icons-material/Send';
+import ThumbUpAltIcon from '@mui/icons-material/ThumbUpAlt';
 
 const Main = (props) => {
   return (
@@ -36,7 +40,57 @@ const Main = (props) => {
 
       <div>
         <Article>
-          Article
+          <SharedActor>
+            <a>
+              <img src="/images/user.svg" alt="" />
+              <div>
+                <span>Title</span>
+                <span>Info</span>
+                <span>Date</span>
+              </div>
+            </a>
+            <button>
+              <MoreHorizIcon />
+            </button>
+          </SharedActor>
+          <Description>
+            Description
+          </Description>
+          <SharedImg>
+            <a>
+              <img src="/images/feed1.jpg" alt="" />
+            </a>
+          </SharedImg>
+          <SocialCounts>
+            <li>
+              <button>
+                <img src="https://static-exp1.licdn.com/sc/h/d310t2g24pvdy4pt1jkedo4yb" alt="" />
+                <img src="https://static-exp1.licdn.com/sc/h/5thsbmikm6a8uov24ygwd914f" alt="" />
+                <span>75</span>
+              </button>
+            </li>
+            <li>
+              <a>2 comments</a>
+            </li>
+          </SocialCounts>
+          <SocialActions>
+          <button>
+            <ThumbUpAltIcon />
+            <span>Like</span>
+          </button>
+          <button>
+            <InsertCommentIcon />
+            <span>Comments</span>
+          </button>
+          <button>
+            <InsertCommentIcon />
+            <span>Share</span>
+          </button>
+          <button>
+            <InsertCommentIcon />
+            <span>Send</span>
+          </button>
+          </SocialActions>
         </Article>
       </div>
     </Container>
@@ -120,5 +174,118 @@ const Article = styled(CommonCard)`
   margin: 0 0 8px;
   overflow: visible;
 `;
+
+const SharedActor = styled.div`
+  padding-right: 40px;
+  flex-wrap: nowrap;
+  padding: 12px 16px 0;
+  margin-bottom: 8px;
+  align-items: center;
+  display: flex;
+  a {
+    margin-right: 12px;
+    flex-grow: 1;
+    overflow: hidden;
+    display: flex;
+    text-decoration: none;
+
+    img {
+      width: 48px;
+      height: 48px;
+    }
+    & > div {
+      display: flex;
+      flex-direction: column;
+      flex-grow: 1;
+      flex-basis: 0;
+      margin-left: 8px;
+      overflow: hidden;
+      span {
+        text-align: left;
+        &:first-child {
+          font-size: 14px;
+          font-weight: 700;
+          color: rgba(0, 0, 0, 1);
+        }
+
+        &:nth-child(n + 1) {
+          font-size: 12px;
+          color: rgba(0, 0, 0, 0.6);
+        }
+      }
+    }
+  }
+
+  button {
+    position: absolute;
+    right: 12px;
+    top: 0;
+    background: transparent;
+    border: none;
+    outline: none;
+  }
+`;
+
+const Description = styled.div`
+  padding: 0 16px;
+  overflow: hidden;
+  color: rgba(0, 0, 0, 0.9);
+  font-size: 14px;
+  text-align: left;
+`;
+
+const SharedImg = styled.div`
+  margin-top: 8px;
+  width: 100%;
+  display: block;
+  position: relative;
+  background-color: #f9fafb;
+  img {
+    object-fit: contain;
+    width: 100%;
+    height: 100%;
+  }
+`;
+
+const SocialCounts = styled.ul`
+  line-height: 1.3;
+  display: flex;
+  align-items: flex-start;
+  overflow: auto;
+  margin: 0 16px;
+  padding: 8px 0;
+  border-bottom: 1px solid #e9e5df;
+  list-style: none;
+  li {
+    margin-right: 5px;
+    font-size: 12px;
+    button {
+      display: flex;
+    }
+  }
+`;
+
+const SocialActions = styled.div`
+  align-items: center;
+  display: flex;
+  justify-content: flex-start;
+  margin: 0;
+  min-height: 40px;
+  padding: 4px 8px;
+  button {
+    display: inline-flex;
+    align-items: center;
+    padding: 8px;
+    color: #0a66c2;
+
+    @media (min-width: 768px) {
+      span {
+        margin-left: 8px;
+      }
+    }
+  }
+`;
+
+
 
 export default Main
